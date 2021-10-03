973c835e8bb8f63ea0d669427ae345b749015d69
Matias Rojas <saitamsajor@gmail.com>
2021-10-03 11:58:06 +0200

    Add files via upload

--- starter-code-sprint3-master/js/checkout.js
+++ starter-code-sprint3-master/js/checkout.js
@@ -1,14 +1,261 @@
-// Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
-
-// Get the error elements
-var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
-
 // Exercise 9
-function validate() {
-    // Validate fields entered by the user: name, phone, password, and email
+function validate() { 
+//expressions regulars utilitzades per validar diferrents dades
+var checkLetras=/^[a-zA-Z\s]*$/; //validar només lletres majúscules i minúscules
+var checkNum=/^\d*$/;//validar només números 
+var checkPass=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/;//validar números i lletres majúscules i minúscules
+var checkEmail=/\w+@\w+\.+[a-z]/;//valida email(números o lletres+ @ + números o lletres + punt + lletres minúscules)
+
+var email = document.querySelector(".email");
+var password = document.querySelector(".password");
+var address = document.querySelector(".address");
+var phone = document.querySelector('.phone');
+var name = document.querySelector(".name");
+var lastName = document.querySelector(".lastName"); 
+var errorName = document.getElementById("errorName").innerHTML;
+var errorPhone = document.getElementById('errorPhone').innerHTML;
+var errorPassword = document.getElementById("errorPassword").innerHTML;
+
+
+   //name
+   
+   //last name   
+   
+  
+ 
+ 
+
+//name
+   if(name.value.length==0){
+      console.log('pop');
+      document.getElementById("wName").classList.add('input-error-active');
+      name.nextElementSibling.classList.add('mensaje-activo');
+      name.nextElementSibling.textContent='All camps are required';
+      
+      return false;
+      }else{
+         document.getElementById("wName").classList.remove('input-error-active');
+         name.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wName").classList.add('input');
+      }
+
+
+
+
+   if(name.value.length<3){
+      console.log('pop');
+      document.getElementById("wName").classList.add('input-error-active');
+      name.nextElementSibling.classList.add('mensaje-activo');
+      name.nextElementSibling.textContent='At least 3 digits required';
+      
+      return false;
+      }else{
+         document.getElementById("wName").classList.remove('input-error-active');
+         name.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wName").classList.add('input');
+      }
+
+   if(name.value.search(checkLetras)){
+         
+      document.getElementById("wName").classList.add('input-error-active');
+      name.nextElementSibling.classList.add('mensaje-activo')
+      name.nextElementSibling.textContent='Numbers are forbidden';
+      
+      return false;
+      }else{
+         document.getElementById("wName").classList.remove('input-error-active');
+         name.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wName").classList.add('input')
+
+      } 
+   //email
+   if(email.value.length==0){
+      console.log('pop');
+      document.getElementById("wEmail").classList.add('input-error-active');
+      email.nextElementSibling.classList.add('mensaje-activo');
+      email.nextElementSibling.textContent='All camps are required';
+      
+      return false;
+      }else{
+         document.getElementById("wEmail").classList.remove('input-error-active');
+         email.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wEmail").classList.add('input');
+      }
+
+   if(email.value.search(checkEmail)){
+      document.getElementById("wEmail").classList.add('input-error-active');
+      email.nextElementSibling.classList.add('mensaje-activo');
+      email.nextElementSibling.textContent='Enter a valid email address';
+      
+      return false;
+      }else{
+         document.getElementById("wEmail").classList.remove('input-error-active');
+         email.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wEmail").classList.add('input');
+   
+      }
+   
+  //address
+   if(address.value.length==0){
+      console.log('pop');
+      document.getElementById("wAddress").classList.add('input-error-active');
+      address.nextElementSibling.classList.add('mensaje-activo');
+      address.nextElementSibling.textContent='All camps are required';
+      
+      return false;
+      }else{
+         document.getElementById("wAddress").classList.remove('input-error-active');
+         address.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wAddress").classList.add('input');
+      }
+
+   if(address.value.length<3){
+      console.log('pop');
+      document.getElementById("wAddress").classList.add('input-error-active');
+      address.nextElementSibling.classList.add('mensaje-activo');
+      address.nextElementSibling.textContent='At least 3 digits required';
+      
+      return false;
+      }else{
+         document.getElementById("wAddress").classList.remove('input-error-active');
+         address.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wAddress").classList.add('input');
+      }
+      //last name
+   if(lastName.value.length==0){
+      console.log('pop');
+      document.getElementById("wLastName").classList.add('input-error-active');
+      lastName.nextElementSibling.classList.add('mensaje-activo');
+      lastName.nextElementSibling.textContent='All camps are required';
+      
+      return false;
+      }else{
+         document.getElementById("wLastName").classList.remove('input-error-active');
+         lastName.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wLastName").classList.add('input');
+      }
+
+   if(lastName.value.length<3){
+      console.log('pop');
+      document.getElementById("wLastName").classList.add('input-error-active');
+      lastName.nextElementSibling.classList.add('mensaje-activo');
+      lastName.nextElementSibling.textContent='At least 3 digits required';
+      
+      return false;
+      }else{
+         document.getElementById("wLastName").classList.remove('input-error-active');
+         lastName.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wLastName").classList.add('input');
+      }
+
+   if(lastName.value.search(checkLetras)){
+
+      document.getElementById("wLastName").classList.add('input-error-active');
+      lastName.nextElementSibling.classList.add('mensaje-activo');
+      lastName.nextElementSibling.textContent='Numbers are forbidden';
+      
+      return false;
+      }else{
+         document.getElementById("wLastName").classList.remove('input-error-active');
+         lastName.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wLastName").classList.add('input');
+   
+      }
+   //password
+   if(password.value.length==0){
+      console.log('pop');
+      document.getElementById("wPassword").classList.add('input-error-active');
+      password.nextElementSibling.classList.add('mensaje-activo');
+      password.nextElementSibling.textContent='All camps are required';
+      
+      return false;
+      }else{
+         document.getElementById("wPassword").classList.remove('input-error-active');
+         password.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wPassword").classList.add('input');
+      }
+
+
+   if(password.value.length<3){
+      console.log('pop');
+      document.getElementById("wPassword").classList.add('input-error-active');
+      password.nextElementSibling.classList.add('mensaje-activo');
+      password.nextElementSibling.textContent='At least 3 digits required';
+      
+      return false;
+      }else{
+         document.getElementById("wPassword").classList.remove('input-error-active');
+         password.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wPassword").classList.add('input');
+      }
+   if(password.value.search(checkPass)){
+      document.getElementById("wPassword").classList.add('input-error-active');
+      password.nextElementSibling.classList.add('mensaje-activo');
+      password.nextElementSibling.textContent='Numbers and letters needed';
+      
+      return false;
+      }else{
+         document.getElementById("wPassword").classList.remove('input-error-active');
+         password.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wPassword").classList.add('input');
+   
+      }
+   
+   //phone
+   if(phone.value.search(checkNum)){
+      document.getElementById("wPhone").classList.add('input-error-active');
+      phone.nextElementSibling.classList.add('mensaje-activo');
+      phone.nextElementSibling.textContent='Just numbers';
+      
+      return false;
+      }else{
+         document.getElementById("wPhone").classList.remove('input-error-active');
+         phone.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wPhone").classList.add('input');
+   
+      }
+
+   if(phone.value.length==0){
+      console.log('pop');
+      document.getElementById("wPhone").classList.add('input-error-active');
+      phone.nextElementSibling.classList.add('mensaje-activo');
+      phone.nextElementSibling.textContent='All camps are required';
+      
+      return false;
+      }else{
+         document.getElementById("wPhone").classList.remove('input-error-active');
+         phone.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wPhone").classList.add('input');
+   
+      }
+   
+
+   if(phone.value.length<3){
+      console.log('pop');
+      document.getElementById("wPhone").classList.add('input-error-active');
+      phone.nextElementSibling.classList.add('mensaje-activo');
+      phone.nextElementSibling.textContent='At least 3 digits required';
+      
+      return false;
+      }else{
+         document.getElementById("wPhone").classList.remove('input-error-active');
+         phone.nextElementSibling.classList.remove('mensaje-activo');
+         document.getElementById("wPhone").classList.add('input');
+         return false;
+   
+      }
+
+   
+
+   
+    
+   /*if(name.value.length==0||lastName.value.length==0||email.value.length==0||password.value.length==0||address.value.length==0||phone.value.length==0){
+      alert('All camps required')
+      //document.getElementById("mensaje").classList.add('mensaje-activo');
+      return false;
+   }*/
+   
+   
+    
 }
+    // Validate fields entered by the user: name, phone, password, and email
--- starter-code-sprint3-master/js/grocery.js
+++ starter-code-sprint3-master/js/grocery.js
@@ -1,5 +1,3 @@
-// Exercise 11
-// Move this variable to a json file and load the data in this js
 var products = [
     {
         name: 'cooking oil',
@@ -47,6 +45,7 @@ var products = [
         type: 'clothes'
     }
 ]
+
 var cartList = [];
 var cart = [];
 var subtotal = {
@@ -63,58 +62,198 @@ var subtotal = {
         discount: 0
     },
 };
+
 var total = 0;
+var preuTotal=0;
+var tipus;
+var subtotalGrocery=0;
+var subtotalClothes=0;
+var subtotalBeauty=0;
 
 // Exercise 1
+// 1. Loop for to the array products to get the item to add to cart
+function mostraProductes(products){
+    for(let i=0; i<products.length; i++) {
+        products[i].id=1+i;
+        console.log('Producte número '+(i+1));
+        console.log(products[i]);
+    }
+}
+ // 2. Add found product to the cartList array
+
 function addToCartList(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    cartList.push(products[id-1]);
 }
 
 // Exercise 2
 function cleanCart() {
-
+    cartList=[];
 }
 
 // Exercise 3
-function calculateSubtotals() {
     // 1. Create a for loop on the "cartList" array 
     // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+
+function calculateSubtotals(cartList) {
+    for (i=0; i<cartList.length; i++) {
+        tipus=cartList[i].type;
+        cartList.map(v => Object.assign(v, {quantity: 1}));//afegim atribut quantity a tots els objectes de cartList amb valor 1
+        if (tipus=='grocery'){
+            subtotal.grocery.value+=cartList[i].price;
+        
+        }else if(tipus=='beauty'){
+            subtotal.beauty.value+=cartList[i].price;
+        }else{
+            subtotal.clothes.value+=cartList[i].price;
+        }
+    }
+    console.log('Subtotals:')
+    console.log('  Grocery: '+subtotal.grocery.value);
+    console.log('  Beauty: '+subtotal.beauty.value);
+    console.log('  Clothes: '+subtotal.clothes.value);
 }
+
 
 // Exercise 4
-function calculateTotal() {
     // Calculate total price of the cart either using the "cartList" array
+//versió 1 comptant directe a l'array amb un for
+function calculateTotal(cartList) {
+    for (i=0; i<cartList.length; i++) {
+        preuTotal+=cartList[i].price;
+    }
+    console.log('Total: '+preuTotal);
 }
+
+//versió 2  comptant subtotals amb un for...in
+function calculateTotal2(cartList) {
+    calculateSubtotals(cartList);
+    var preuTotal2=0;
+    for (var x in subtotal) {
+        preuTotal2+=subtotal[x].value;
+    }
+    console.log('Total: '+preuTotal2);
+}
+
 
 // Exercise 5
-function applyPromotionsSubtotals() {
-
-}
+// versió 1 descompte per tipus de producte en general(grocery, beauty o clothes)
+function applyPromotionsSubtotals(array,kind,discount) {//(array on treballar, tipus de producte, descompte)
+    var promoSubTotal=0;
+    for (let i=0; i<array.length; i++) {
+        var indice= array[i];
+        tipus=indice.type;
+        if (tipus==kind){
+        promoSubTotal+= indice.price;
+        }
+    }console.log(promoSubTotal-(promoSubTotal*discount)/100);
+} 
 
 // Exercise 6
-function generateCart() {
+/*function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+}*/
+function generateCart(cartList){//afegit el paràmetre cartlist a la funció per poder probar-la amb un altre array sense modificar el codi
+    let set= new Set(cartList.map( JSON.stringify ))//convertits els objectes a cadena de text per comprobar repetits
+    cart = Array.from( set ).map( JSON.parse );//afegim a cart els resultats únics i convertim els strings d'abans a objectes
+    
+    for( let i=0; i<cart.length;i++){
+        cart[i].quantity=0;// reset del camp a 0 per anar sumant els que venen de cartList
+        for( let j=0; j<cartList.length; j++ ){
+            
+            if(cart[i].name==cartList[j].name){
+                cart[i].quantity++;
+                cart[i].subtotal=(cart[i].quantity*cart[i].price);
+            }
+        }
+    }
+    for( let i=0; i<cart.length;i++){
+        if(cart[i].name=='cooking oil'&cart[i].quantity>=3){
+            cart[i].subtotalWithDiscount=cart[i].quantity*10; //afegim l'atribut subtotalWithDiscount per l'oferta a l'oli
+        }else if(cart[i].name=='Instant cupcake mixture'&cart[i].quantity>=10){
+            cart[i].subtotalWithDiscount=(cart[i].subtotal*2)/3; //afegim l'atribut subtotalWithDiscount per l'oferta a la mescla per pastís
+        }else{cart[i].subtotalWithDiscount='No discount'} //afegim l'atribut subtotalWithDiscount sense discount per a la resta de productes 
+    }
+    console.log(cart);
 }
+
+//Funció per aplicar les promos que diu l'exercici en comptes d'afegir el codi dins la propia funció de generateCart
+function promoOilAndCake(cart){
+    for( let i=0; i<cart.length;i++){
+        if(cart[i].name=='cooking oil'&cart[i].quantity>=3){
+            cart[i].subtotalWithDiscount=cart[i].quantity*10;
+        }else if(cart[i].name=='Instant cupcake mixture'&cart[i].quantity>=10){
+            cart[i].subtotalWithDiscount=(cart[i].subtotal*2)/3;
+        }else{cart[i].subtotalWithDiscount='No discount'}
+    }
+}
+
+
+
+
 
 // Exercise 7
-function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
+
+//Segons  l'exercici del moddle s'aplicaven despcomptes per categoria però l'única promo que s'ha demanat era sobre producte...
+
+//funció per aplicar descomptes al total de la compra
+function fullDiscount(discount, array){
+    calculateTotal(array);
+    console.log(preuTotal-(preuTotal*discount)/100);
 }
+
+// Apply promotions to each item in the array "cart"
+
+function applyPromotionsCart(productName, discount, num,){
+    for( let i=0; i<cart.length;i++){
+        if(cart[i].name==productName&cart[i].quantity>=num){
+            cart[i].subtotalWithDiscount=cart[i].subtotal-(cart[i].subtotal*discount)/100;
+        }else{cart[i].subtotalWithDiscount='No discount'}
+    }
+}
+
+
+
+
 
 // Exercise 8
-function addToCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+ // 1. Loop for to the array products to get the item to add to cart
+// 2. Add found product to the cartList array
+//console.log('Quin producte vols afegir?:');
+//mostraProductes(products);//retorna l'array products amb un número d'id
+function addToCart(id){
+    var troba=false;
+    if(cart.length==0){
+        cart.push(products[id-1]);
+        cart[0].quantity=1;
+    }
+    else if(cart.length>0){
+        for(var i=0; i<cart.length; i++){
+            if(products[id-1].name==cart[i].name){
+                troba=true;}
+                if(troba){
+                    cart[i].quantity++;}
+        }
+        if(troba==false){
+            cart.push(products[id-1]);
+            cart[cart.length-1].quantity=1;
+        }
+    }
+    for( let i=0; i<cart.length; i++){
+        cart[i].subtotal=cart[i].quantity*cart[i].price;
+    }
 }
+
+
 
 // Exercise 9
+// 1. Loop for to the array products to get the item to add to cart
+console.log('Quin producte vols esborrar?:');
+mostraProductes(cart);//retorna l'array products amb un número d'id
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+    cart.splice(id-1,1);
 }
-
 
 
 // Exercise 10
