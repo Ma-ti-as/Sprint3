1dad97c48c212410aa24a125343e61e120d17599
Matias Rojas <saitamsajor@gmail.com>
2021-10-05 15:38:06 +0200

    Add files via upload
    afegit l'arxiu grocery(nou) i canviat a groceryOld(l'original) pels canvis als enunciats de l'sprint3

--- starter-code-sprint3-master/js/groceryOld.js
+++ starter-code-sprint3-master/js/groceryOld.js
@@ -1,5 +1,3 @@
-// Exercise 10
-// Move this variable to a json file and load the data in this js
 var products = [
     {
         name: 'cooking oil',
@@ -47,6 +45,7 @@ var products = [
         type: 'clothes'
     }
 ]
+
 var cartList = [];
 var cart = [];
 var subtotal = {
@@ -63,6 +62,7 @@ var subtotal = {
         discount: 0
     },
 };
+
 var total = 0;
 var preuTotal=0;
 var tipus;
@@ -71,8 +71,7 @@ var subtotalClothes=0;
 var subtotalBeauty=0;
 
 // Exercise 1
-    // 1. Loop for to the array products to get the item to add to cart
-
+// 1. Loop for to the array products to get the item to add to cart
 function mostraProductes(products){
     for(let i=0; i<products.length; i++) {
         products[i].id=1+i;
@@ -80,44 +79,37 @@ function mostraProductes(products){
         console.log(products[i]);
     }
 }
-mostraProductes(products);
-    // 2. Add found product to the cartList array
+ // 2. Add found product to the cartList array
 
 function addToCartList(id) {
     cartList.push(products[id-1]);
 }
-addToCartList(1);
-addToCartList(1);
-addToCartList(1);
-addToCartList(1);
-addToCartList(2);
-addToCartList(3);
-addToCartList(3);
-addToCartList(3);
-addToCartList(3);
-addToCartList(3);
-addToCartList(3);
-addToCartList(3);
-addToCartList(3);
-addToCartList(3);
-addToCartList(3);
 
-addToCartList(4);
-addToCartList(5);
-console.log(cartList)
-
-
-// Exercise 2
-function cleanCart() {
+// Exercise 2  Segons enunciat s'ha de buidar cartList.
+function cleanCart1() {
     cartList=[];
 }
 
+addToCartList(1);
+addToCartList(2);
+addToCartList(3);
+console.log(cartList.length);
+console.log(cartList);
+cleanCart1();
+console.log(cartList.length);
+console.log(cartList);
+
+
+//Però si es vol buidar qualsevol array es passa per paràmetre l'array a buidar
+function cleanCart2(cartList){
+    cartList=[];
+}
 
 // Exercise 3
     // 1. Create a for loop on the "cartList" array 
     // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
 
-function calculateSubtotals() {
+function calculateSubtotals(cartList) {
     for (i=0; i<cartList.length; i++) {
         tipus=cartList[i].type;
         cartList.map(v => Object.assign(v, {quantity: 1}));//afegim atribut quantity a tots els objectes de cartList amb valor 1
@@ -134,25 +126,49 @@ function calculateSubtotals() {
     console.log('  Grocery: '+subtotal.grocery.value);
     console.log('  Beauty: '+subtotal.beauty.value);
     console.log('  Clothes: '+subtotal.clothes.value);
-    
-};
+}
+
 
 // Exercise 4
     // Calculate total price of the cart either using the "cartList" array
-
-function calculateTotal() {
-    calculateSubtotals();
-    for (var x in subtotal) {
-        preuTotal+=subtotal[x].value;
+//versió 1 comptant directe a l'array amb un for
+function calculateTotal(cartList) {
+    for (i=0; i<cartList.length; i++) {
+        preuTotal+=cartList[i].price;
     }
     console.log('Total: '+preuTotal);
-}calculateTotal()
+}
+
+//versió 2  comptant subtotals amb un for...in
+function calculateTotal2(cartList) {
+    calculateSubtotals(cartList);
+    var preuTotal2=0;
+    for (var x in subtotal) {
+        preuTotal2+=subtotal[x].value;
+    }
+    console.log('Total: '+preuTotal2);
+}
+
 
 // Exercise 5
+// versió 1 descompte per tipus de producte en general(grocery, beauty o clothes)
+function applyPromotionsSubtotals(array,kind,discount) {//(array on treballar, tipus de producte, descompte)
+    var promoSubTotal=0;
+    for (let i=0; i<array.length; i++) {
+        var indice= array[i];
+        tipus=indice.type;
+        if (tipus==kind){
+        promoSubTotal+= indice.price;
+        }
+    }console.log(promoSubTotal-(promoSubTotal*discount)/100);
+} 
+
+// Exercise 6
+/*function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-
-function generateCart(){//afegit el paràmetre cartlist a la funció per poder probar-la amb un altre array sense modificar el codi
+}*/
+function generateCart(cartList){//afegit el paràmetre cartlist a la funció per poder probar-la amb un altre array sense modificar el codi
     let set= new Set(cartList.map( JSON.stringify ))//convertits els objectes a cadena de text per comprobar repetits
     cart = Array.from( set ).map( JSON.parse );//afegim a cart els resultats únics i convertim els strings d'abans a objectes
     
@@ -165,13 +181,7 @@ function generateCart(){//afegit el paràmetre cartlist a la funció per poder p
                 cart[i].subtotal=(cart[i].quantity*cart[i].price);
             }
         }
-    }console.log(cart);
-}generateCart()
-
-// Exercise 6
-    // Apply promotions to each item in the array "cart"
-
-function applyPromotionsCart() {
+    }
     for( let i=0; i<cart.length;i++){
         if(cart[i].name=='cooking oil'&cart[i].quantity>=3){
             cart[i].subtotalWithDiscount=cart[i].quantity*10; //afegim l'atribut subtotalWithDiscount per l'oferta a l'oli
@@ -180,14 +190,53 @@ function applyPromotionsCart() {
         }else{cart[i].subtotalWithDiscount='No discount'} //afegim l'atribut subtotalWithDiscount sense discount per a la resta de productes 
     }
     console.log(cart);
-    
-}applyPromotionsCart()
+}
+
+//Funció per aplicar les promos que diu l'exercici en comptes d'afegir el codi dins la propia funció de generateCart
+function promoOilAndCake(cart){
+    for( let i=0; i<cart.length;i++){
+        if(cart[i].name=='cooking oil'&cart[i].quantity>=3){
+            cart[i].subtotalWithDiscount=cart[i].quantity*10;
+        }else if(cart[i].name=='Instant cupcake mixture'&cart[i].quantity>=10){
+            cart[i].subtotalWithDiscount=(cart[i].subtotal*2)/3;
+        }else{cart[i].subtotalWithDiscount='No discount'}
+    }
+}
+
+
+
+
 
 // Exercise 7
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array
 
-function addToCart(id) {
+//Segons  l'exercici del moddle s'aplicaven despcomptes per categoria però l'única promo que s'ha demanat era sobre producte...
+
+//funció per aplicar descomptes al total de la compra
+function fullDiscount(discount, array){
+    calculateTotal(array);
+    console.log(preuTotal-(preuTotal*discount)/100);
+}
+
+// Apply promotions to each item in the array "cart"
+
+function applyPromotionsCart(productName, discount, num,){
+    for( let i=0; i<cart.length;i++){
+        if(cart[i].name==productName&cart[i].quantity>=num){
+            cart[i].subtotalWithDiscount=cart[i].subtotal-(cart[i].subtotal*discount)/100;
+        }else{cart[i].subtotalWithDiscount='No discount'}
+    }
+}
+
+
+
+
+
+// Exercise 8
+ // 1. Loop for to the array products to get the item to add to cart
+// 2. Add found product to the cartList array
+//console.log('Quin producte vols afegir?:');
+//mostraProductes(products);//retorna l'array products amb un número d'id
+function addToCart(id){
     var troba=false;
     if(cart.length==0){
         cart.push(products[id-1]);
@@ -207,21 +256,22 @@ function addToCart(id) {
     }
     for( let i=0; i<cart.length; i++){
         cart[i].subtotal=cart[i].quantity*cart[i].price;
-    }applyPromotionsCart();
-    
+    }
 }
 
-// Exercise 8
-    // 1. Loop for to the array products to get the item to add to cart
-    mostraProductes(cart);
-    // 2. Remove found product to the cartList array
 
-function removeFromCart(id) {
-    cart.splice(id-1,1);
-    console.log(cart);
-}
 
 // Exercise 9
+// 1. Loop for to the array products to get the item to add to cart
+//console.log('Quin producte vols esborrar?:');
+mostraProductes(cart);//retorna l'array products amb un número d'id
+function removeFromCart(id) {
+    // 2. Add found product to the cartList array
+    cart.splice(id-1,1);
+}
+
+
+// Exercise 10
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
 }
